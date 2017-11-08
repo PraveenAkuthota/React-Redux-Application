b6365b5ff9c11f9ef34ead2facd3c49cbfb59135
praveenaku1234 <praveenaku1234@gmail.com>
2017-11-08 01:56:21 -0500

    Setting up the initial reducer books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+    return[
+        { title : 'Prison Break'},
+        { title : 'Breaking bad'},
+        { title : 'Stranger Things'},
+        { title : 'Narcos'}
+    ]
+}
+
